36c7d9328e208819ef0efe0fea2b8ccfb317c224
unexCoder <unexCoder@unexCoder.local>
2025-11-19 18:45:26 -0300

    nuevo cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,6 +5,11 @@ const Cart = () => {
         <div>
             <h1>Carrito</h1>
             <p>En esta vista mostramos el carrito de copmpras.</p>
+            <p>En esta vista mostramos el carrito de copmpras.</p>
+            <p>En esta vista mostramos el carrito de copmpras.</p>
+            <p>En esta vista mostramos el carrito de copmpras.</p>
+            <p>En esta vista mostramos el carrito de copmpras.</p>
+            <p>En esta vista mostramos el carrito de copmpras.</p>
         </div>
     );
 };
