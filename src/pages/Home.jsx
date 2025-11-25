5e2d192797d4c133c57e989a095c00fd5d43328e
unexCoder <unexCoder@unexCoder.local>
2025-11-25 13:01:14 -0300

    final upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,10 @@ import teamImage from '../assets/team.jpg';
 
 const Home = () => {
     return (
-        <div>
+        <div style={{display:'flex',flexDirection:'column', padding: '20px' }}>
             <h1>Bienvenidos!</h1>
-            <h2>Aplicacion web para punto de ventas, desarrollada durante el taller Desarrollo Frontend en Puerto Jóven.</h2>
-            <img src={teamImage} alt="team" style={{width:'80svw'}} />
+            <h2>Aplicacion web para punto de ventas, desarrollada durante el taller Desarrollo Frontend en Puerto Jóven. Equipo de desarrollo: Jorgelina Locardel, Maia Branchesi, Alex Burgueńo, Lorenzo Martín Prince, Carolina Valentini, Silvana Hernández. Docente: Luigi Tamagnini</h2>
+            <img src={teamImage} alt="team" style={{width:'75svw',height:'auto',justifySelf:'center',alignSelf:'center'}} />
         </div>
     );
 };
